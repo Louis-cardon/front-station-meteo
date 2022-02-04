937611330ab1036e0ec2344bd8b31318c68585b4
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2022-02-04 11:57:00 +0100

    test

--- src/store/index.js
+++ src/store/index.js
@@ -7,17 +7,27 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     connected: false,
-    userInfo: []
+    userInfo: [],
+    weatherJson: []
   },
   getters:{
-    connected: state => state.connected
+    connected: state => state.connected,
+    weatherJson: state => state.weatherJson
   },
   mutations: {
     setConnected(state, bool) {
       state.connected = bool
+    },
+    setWeather(state, weatherJson) {
+      state.weatherJson = weatherJson
     }
   },
   actions: {
+    async getmeteo(state, ville){
+      const weather = await axios.get("http://api.openweathermap.org/data/2.5/find?q="+ville+"&appid=9def32b346e03faf0e077a943a7e8bde&lang=fr")
+      console.log(weather.data)
+      state.commit("setWeather",weather.data.list[0].weather[0])
+    },
     async connect(state) {
       state.commit('setConnected', true)
     }
